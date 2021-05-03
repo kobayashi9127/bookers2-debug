17d6cb51ec1881173963ccaa12f0f0e1d7d51d6d
kobayashi9127 <bacchi.9127@gmail.com>
2021-05-03 12:02:25 +0000

    [fin]3

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'homes#top'
-  resources :users,only: [:show,:index,:edit,:update]
-  resources :books
   get 'home/about' => 'homes#about'
-  
-end 
+  resources :users,only: [:show,:index,:edit,:update]
+  resources :books do
+   resources :book_comments, only: [:create, :destroy]      
+   resource :favorites, only: [:create, :destroy]
+ end
+ 
+end
