780417f7efbd50701468a2cc70adf8b65831007c
kobayashi9127 <bacchi.9127@gmail.com>
2021-05-04 11:06:38 +0000

    [編集]

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,34 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :books, dependent: :destroy
-  has_many :favorites, dependent: :destroy
-  has_many :book_comments, dependent: :destroy
   attachment :profile_image, destroy: false
 
   validates :name, length: {maximum: 20, minimum: 2}, uniqueness: true
   validates :introduction, length: {maximum: 50}
-  
+
+  has_many :favorites, dependent: :destroy
+  has_many :book_comments, dependent: :destroy
+
+  # 自分がフォローされる（被フォロー）側の関係性
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  # 自分がフォローする（与フォロー）側の関係性
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  # 被フォロー関係を通じて参照→自分をフォローしている人
+  has_many :followers, through: :reverse_of_relationships, source: :follower
+  # 与フォロー関係を通じて参照→自分がフォローしている人
+  has_many :followings, through: :relationships, source: :followed
+
+  # ユーザーをフォローする
+def follow(user_id)
+  relationships.create(followed_id: user_id)
+end
+# ユーザーのフォローを外す
+def unfollow(user_id)
+  relationships.find_by(followed_id: user_id).destroy
+end
+# フォローしていればtrueを返す
+def following?(user)
+  followings.include?(user)
+end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,14 @@ Rails.application.routes.draw do
   devise_for :users
   root 'homes#top'
   get 'home/about' => 'homes#about'
-  resources :users,only: [:show,:index,:edit,:update]
+  resources :users,only: [:show,:index,:edit,:update] do
+   
+   get 'followings' => 'relationships#followings', as: 'followings'
+   get 'followers' => 'relationships#followers', as: 'followers'
+   end
   resources :books do
    resources :book_comments, only: [:create, :destroy]      
    resource :favorites, only: [:create, :destroy]
- end
+   end
  
 end
